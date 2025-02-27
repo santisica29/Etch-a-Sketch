84c411a6e228d8a3cf00db2d1d94d9291ba5c0ad
Santiago Sica <santisica29@gmail.com>
2025-02-27 13:00:05 -0300

    made grid scalable and added mouseover color function

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
-const container = document.querySelector('.container');
-const containerWidth = container.clientWidth;
-
+const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 createGrid();
 
-function createGrid(squares = 16){
-    container.innerHTML = "";
+btn.addEventListener("click", createNewGrid);
 
-    let numOfSquares = squares * squares;
-    let sizeOfSquares = containerWidth / squares;
-    
-    for (let i = 1; i <= numOfSquares; i++){
-        const grid = document.createElement('div');
-        grid.style.width = `${sizeOfSquares}px`;
-        grid.style.height = `${sizeOfSquares}px`;
+function createNewGrid() {
+  let flag = false;
+  let newGridSize = 0;
 
-        grid.classList.add('square');
-        container.appendChild(grid);
+  while (flag === false) {
+    newGridSize = prompt("num of squares? (1-100)");
 
+    if (newGridSize > 100 || newGridSize < 1) {
+      alert("invalid number, try again");
+    } else {
+      flag = true;
     }
+  }
+
+  createGrid(newGridSize);
+}
+createGrid();
+
+function createGrid(squares = 16) {
+  container.innerHTML = "";
+
+  let numOfSquares = squares * squares;
+
+  for (let i = 1; i <= numOfSquares; i++) {
+    const grid = document.createElement("div");
+    grid.style.setProperty("min-width", `calc(100% / ${squares})`);
+
+    grid.classList.add("square");
+    container.appendChild(grid);
+  }
+
+  const grids = document.querySelectorAll(".square");
+  grids.forEach((sq) => {
+    sq.addEventListener("mouseover", (event) => {
+      event.target.style.background = "lightblue";
+    });
+    console.log(1);
+  });
 }
